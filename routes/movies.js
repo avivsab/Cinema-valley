9f15461d6037b864a5f613c9544c6704e104357e
avivsab <avivsab19@gmail.com>
2019-05-16 19:10:50 +0300

    Create server for the app with connection to sql db

---
+++ routes/movies.js
@@ -0,0 +1,68 @@
+const mysql = require('mysql2/promise');
+const express = require('express');
+const bodyParser = require('body-parser')
+const router = express.Router();
+router.use(bodyParser.json());
+
+router.get('/', async (req,res) => {
+    const connection = await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '12345678',
+        database: 'cinemaville'
+      });
+   const [results, fields] = await connection.execute('select * from Movies');
+   res.send(results);
+   
+
+
+router.get('/movie_list', async (req,res) => {
+ 
+    const [results, fields] = await connection.execute('select title from Movies');
+    res.send(results);
+})
+
+router.get('/:id', async (req,res) => {
+ 
+      const movieId = req.params.id;
+    const [results, fields] = await connection.execute(`select title from Movies where id = ${movieId}`);
+    res.send(results);
+})
+router.use(express.urlencoded());
+router.use(express.json());
+router.post('/', async (req,res) => {
+    
+    const newMovie = req.body;  
+    // const [results, fields] = await connection.execute(`INSERT INTO Movies (title) VALUES ('${newMovie}')`);
+    console.log(newMovie);  
+    console.log(req)
+});
+
+router.delete('/:id', async (req,res) => {
+ 
+    const delMovie = req.params.id;  
+    const [results, fields] = await connection.execute(`DELETE FROM Movies WHERE id = ${delMovie}`);
+    console.log(delMovie);
+    res.send(results)
+})
+router.delete('/:id', async (req,res) => {
+ 
+    const delMovie = req.params.id;  
+    const [results, fields] = await connection.execute(`DELETE FROM Movies WHERE id = ${delMovie}`);
+    console.log(delMovie);
+    res.send(results)
+})
+
+router.put('/:id', async (req,res) => {
+    
+    const updateMovieId = req.params.id;
+    const updateMovieTitle = req.params.body;  
+    const [results, fields] = await connection.execute(`UPDATE Movies SET title = ${updateMovieTitle} WHERE id = ${updateMovieId}`);
+    console.log(delMovie);
+    res.send(results)
+})
+
+
+
+})
+module.exports = router;
