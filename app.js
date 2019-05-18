922df7a10794410d9a74098f8d198db747e21b3a
avivsab <avivsab19@gmail.com>
2019-05-18 22:06:52 +0300

    Create middleware for 404 err and add designed 404 page template

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ var moviesRouter = require('./routes/movies');
 
 var app = express();
 
+const noPageMidmiddleware = (path) => {
+    return (req, res, next) => {
+        res.sendFile(path);
+    }
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -18,5 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
+app.use(noPageMidmiddleware(path.join(__dirname, 'public', '404.html')));
 
 module.exports = app;
